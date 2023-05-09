419802198ccc3e28ef0fc9cb33030d1ab6980e4d
Paul Vincent C. Mercado <119905612+kuyangpapaul@users.noreply.github.com>
2023-05-10 06:40:42 +0800

    Update Me.java

--- longexam1/Me.java
+++ longexam1/Me.java
@@ -8,64 +8,68 @@ public class Me implements Tourist {
 
     public void visit(Boracay boracay) {
  if (boracay.airFare > budget ){
-        System.out.println("Kulang na pera mo kuys ");
-    }
-    else{
+        System.out.println("Kulang na pera mo kuys, stay ka na lang sa bahay :( ");
+}
+else{
         System.out.println("Andito na ako sa Boracay yeheyyy!!! ");
         budget = budget - boracay.airFare;   
-        }
+}
         checkBudget();
     }
+
     public void visit(Tagaytay tagaytay) {
  if (tagaytay.airFare > budget){
-        System.out.println("Kulang na pera mo kuys ");
-    }
-    else{
+        System.out.println("Kulang na pera mo kuys stay ka na lang sa bahay :( ");
+}
+else{
         System.out.println("Andito na ako sa Tagayatay yeheyyy!!!  ");
         budget = budget - tagaytay.airFare;
-    }
+}
         checkBudget();
     }
+
     public void visit(Lucban lucban) {
   if (lucban.airFare > budget){
-        System.out.println("Kulang na pera mo kuys ");
-    }
-    else{
+        System.out.println("Kulang na pera mo kuys stay ka na lang sa bahay :( ");
+}
+else{
         System.out.println("Andito na ako sa Lucban yeheyyy!!!  ");
         budget = budget - lucban.airFare;
-    }
+}
         checkBudget(); 
     }
+
     public void visit(Palawan palawan) {
   if (palawan.airFare > budget){
-        System.out.println("Kulang na pera mo kuys ");
-    }
-    else{
+        System.out.println("Kulang na pera mo kuys stay ka na lang sa bahay :( ");
+}
+else{
         System.out.println("Andito na ako sa Palawan yeheyyy!!!  ");
         budget = budget - palawan.airFare;
-    }
+}
         checkBudget();
    
     }
+
     public void visit(Cebu cebu) {
    if (cebu.airFare > budget){
-        System.out.println("Kulang na pera mo kuys ");
-    }
-    else{
+        System.out.println("Kulang na pera mo kuys stay ka na lang sa bahay :( ");
+}
+else{
         System.out.println("Andito na ako sa Cebu yeheyyy!!!  ");
         budget = budget - cebu.airFare;
-    }
+}
         checkBudget();
        
     }
     public void visit(PuertoGalera puertoGalera) {
    if (puertoGalera.airFare > budget){
-        System.out.println("Kulang na pera mo kuys ");
-    }
-    else{
+        System.out.println("Kulang na pera mo kuys stay ka na lang sa bahay :( ");
+}
+else{
         System.out.println("Andito na ako sa Puerto Galera  yeheyyy!!!  ");
         budget = budget - puertoGalera.airFare;
-    }
+}
         checkBudget();
         
     }
